089cd63e80cca9d7dc2e897c41ccd7e6c7cbedd4
TheVirus-Limiter <64677380+TheVirus-Limiter@users.noreply.github.com>
2020-11-11 17:14:00 +0530

    Update bob.js

--- bob.js
+++ bob.js
@@ -2,30 +2,30 @@ class bob
 {
 	constructor(x,y,r)
 	{
-		var options={
-			isStatic:false,
-			restitution:1,
-			friction:0,
-			density:0.8
+	var options={
+	isStatic:false,
+		restitution:1,
+	friction:0,
+		density:0.8
 			
 			}
-		this.x=x;
-		this.y=y;
-		this.r=r
+	this.x=x;
+	this.y=y;
+	          	this.r=r
 		
-		this.body=Bodies.circle(this.x, this.y, (this.r)/2, options)
-		World.add(world, this.body);
+	this.body=Bodies.circle(this.x, this.y, (this.r)/2, options)
+	World.add(world, this.body);
 
 	}
 	display()
 	{
 			
-			var paperpos=this.body.position;
-			push()
-			translate(paperpos.x, paperpos.y);
-			rectMode(CENTER)
-			fill(255,0,255)
-			ellipse(0,0,this.r, this.r);
+	var paperpos=this.body.position;
+	push()
+	translate(paperpos.x, paperpos.y);
+	rectMode(CENTER)
+	fill(255,0,255)
+	ellipse(0,0,this.r, this.r);
 			pop()
 			
 	}
